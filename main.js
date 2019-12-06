0b25e5ff58547f6c1654151044298738a2f9c9f8
Zach Zundel <me@zachzundel.com>
2019-12-05 18:32:58 -0700

    pharma

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+// Suppress home menu
+Menu.setApplicationMenu(null);
 
 function createWindow () {
   // Create the browser window.
@@ -17,7 +20,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('layout/home.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
